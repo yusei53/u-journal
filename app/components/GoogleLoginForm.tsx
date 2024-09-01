74a903b1ef6ac1dcfa69397ae72bcf1cd7ea02e5
yusei53 <yusei530922@gmail.com>
2024-09-01 21:22:10 +0900

    feat: GoogleLoginForm.tsxの作成

---
+++ app/components/GoogleLoginForm.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import GoogleLoginButton from "./GoogleLoginButton";
+
+const GoogleLoginForm = () => {
+  return (
+    <Box
+      mt={15}
+      mx={{ md: 25 }}
+      py={5}
+      px={1}
+      border={"1px solid #c4c4c4"}
+      borderRadius={"10px"}
+    >
+      <Typography textAlign={"center"} fontWeight={"bold"} fontSize={20}>
+        ここにアプリ名
+      </Typography>
+      <Typography textAlign={"center"} my={4} fontSize={14}>
+        ログインすると〇〇の機能が使えるようになります
+      </Typography>
+      <GoogleLoginButton />
+    </Box>
+  );
+};
+
+export default GoogleLoginForm;
