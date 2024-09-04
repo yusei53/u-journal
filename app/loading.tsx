91a25ae64b7cf3cd352caf4e85b33b55b5266bf5
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-04 14:50:24 +0900

    style loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,16 @@
+import { Box, CircularProgress } from "@mui/material";
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      height={"60vh"}
+    >
+      <CircularProgress size={"100px"} />
+    </Box>
+  );
 };
 
 export default Loading;
