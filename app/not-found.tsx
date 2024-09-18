0c168b13fb4ef6fbe1842a789619267601999fc0
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-18 13:05:31 +0900

    improve: 不要な空のフラグメント削除

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,30 +2,28 @@ import { Button, Typography, Box } from "@mui/material";
 
 const NotFound = () => {
   return (
-    <>
-      <Box
-        display={"flex"}
-        textAlign={"center"}
-        justifyContent={"center"}
-        flexDirection={"column"}
-        height={"100vh"}
+    <Box
+      display={"flex"}
+      textAlign={"center"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      height={"100vh"}
+    >
+      <Typography fontWeight={"bold"} fontSize={35}>
+        404 Not Found
+      </Typography>
+      <Typography mb={1}>お探しのページは見つかりませんでした</Typography>
+      <Button
+        sx={{
+          backgroundColor: "#13396E",
+          color: "white",
+          alignSelf: "center",
+        }}
+        href="/"
       >
-        <Typography fontWeight={"bold"} fontSize={35}>
-          404 Not Found
-        </Typography>
-        <Typography mb={1}>お探しのページは見つかりませんでした</Typography>
-        <Button
-          sx={{
-            backgroundColor: "#13396E",
-            color: "white",
-            alignSelf: "center",
-          }}
-          href="/"
-        >
-          ホームに戻る
-        </Button>
-      </Box>
-    </>
+        ホームに戻る
+      </Button>
+    </Box>
   );
 };
 
