62de5ba502a2e3b58df270aa6bb743698c21eacf
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-18 12:58:56 +0900

    improve: 全体のスタイル修正

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,4 @@
 import { Button, Typography, Box } from "@mui/material";
-import React from "react";
 
 const NotFound = () => {
   return (
@@ -9,20 +8,20 @@ const NotFound = () => {
         textAlign={"center"}
         justifyContent={"center"}
         flexDirection={"column"}
-        mt={"20vh"}
+        height={"100vh"}
       >
         <Typography fontWeight={"bold"} fontSize={35}>
           404 Not Found
         </Typography>
         <Typography mb={1}>お探しのページは見つかりませんでした</Typography>
-      </Box>
-      <Box
-        display={"flex"}
-        textAlign={"center"}
-        justifyContent={"center"}
-        mt={1}
-      >
-        <Button sx={{ backgroundColor: "#13396E", color: "white" }} href="/">
+        <Button
+          sx={{
+            backgroundColor: "#13396E",
+            color: "white",
+            alignSelf: "center",
+          }}
+          href="/"
+        >
           ホームに戻る
         </Button>
       </Box>
