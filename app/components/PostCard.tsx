f9259d3656ac8acf2d21cc15271ba7739159b073
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-23 13:58:31 +0900

    improve: PoatCard.tsxにpropsをつけた

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -1,12 +1,34 @@
 import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
-const PostCard = () => {
+type PostCardProps = {
+  title: string;
+  day: string;
+  href: string;
+};
+
+const PostCard: React.FC<PostCardProps> = ({ title, day, href }) => {
   return (
-    <>
-      <Box bgcolor={"#f0f7ff"} width={252} height={157} borderRadius={3}></Box>
-      <Typography letterSpacing={2}>題名題名題名題名</Typography>
-      <Typography color="#8D9298">20xx/x/x</Typography>
-    </>
+    <Box marginBottom={3}>
+      <Link href={href}>
+        <Box
+          bgcolor={"#f0f7ff"}
+          width={252}
+          height={157}
+          borderRadius={3}
+        ></Box>
+      </Link>
+      <Box my={1}>
+        <Link href={href} style={{ textDecoration: "none" }}>
+          <Typography letterSpacing={2} color="black">
+            {title}
+          </Typography>
+        </Link>
+      </Box>
+      <Typography color="#8D9298" marginY={1}>
+        {day}
+      </Typography>
+    </Box>
   );
 };
 
