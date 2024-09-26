0395b0090a7968d3aa9808523565178d1833a17c
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-26 12:09:15 +0900

    rename: GoogleLogoutButtonをLogoutButtonに変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import getCurrentUser from "./actions/getCurrentUser";
 import GoogleLoginForm from "./components/GoogleLoginForm";
-import GoogleLogoutButton from "./components/GoogleLogoutButton";
+import LogoutButton from "./components/LogoutButton";
 
 const Home = async () => {
   const currentUser = await getCurrentUser();
   return currentUser ? (
     <>
-      <GoogleLogoutButton />
+      <LogoutButton />
     </>
   ) : (
     <>
