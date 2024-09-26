4789f46d75b26563e6f5c0f3230a247d20fe2155
yusei53 <yusei530922@gmail.com>
2024-09-27 00:09:40 +0900

    improve: prezentationalの責務を持たせる

--- app/components/PostCardArea.tsx
+++ app/components/PostCardArea.tsx
@@ -1,18 +1,43 @@
-import { Box, Grid } from "@mui/material";
-import React from "react";
+import { Box, Grid, Typography } from "@mui/material";
 import PostCard from "./PostCard";
-import { PostcardData } from "../const/PostCardData";
+import { PostCardDataProps } from "../const/PostCardData";
+import UserNameBar from "./UserNameBar";
 
-const PostCardArea = () => {
+type PostCardAreaProps = {
+  postcardData: PostCardDataProps[];
+};
+
+const PostCardArea: React.FC<PostCardAreaProps> = ({ postcardData }) => {
   return (
-    <Box px={{ xs: 10, md: 35 }}>
-      <Grid container>
-        {PostcardData.map((data, index) => (
-          <Grid item xs={12} sm={4} key={index}>
-            <PostCard title={data.title} day={data.day} href={data.href} />
-          </Grid>
-        ))}
-      </Grid>
+    <Box>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        marginY={5}
+        flexDirection={"column"}
+      >
+        <Typography fontSize={20} textAlign={"center"}>
+          振り返り一覧
+        </Typography>
+        <UserNameBar />
+      </Box>
+      <Box px={{ xs: 10, md: 35 }}>
+        <Grid container>
+          {postcardData.map((data, index) => (
+            <Grid item xs={12} sm={4} key={index}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <PostCard postCardData={data} />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   );
 };
