18e33f41864152b7ef8e5bddb4289d1b336dcf90
yusei53 <yusei530922@gmail.com>
2024-10-01 02:39:20 +0900

    fix: typo

--- src/components/reflection/ReflectionPostForm.tsx
+++ src/components/reflection/ReflectionPostForm.tsx
@@ -2,13 +2,13 @@ import { Box, Button, TextField } from "@mui/material";
 import { Controller } from "react-hook-form";
 import Editor from "../markdown-editor/Editor";
 
-type RefrectionPostFormProps = {
+type ReflectionPostFormProps = {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   control: any;
   errors: any;
 };
 
-const ReflectionPostForm: React.FC<RefrectionPostFormProps> = ({
+const ReflectionPostForm: React.FC<ReflectionPostFormProps> = ({
   onSubmit,
   control,
   errors,
