56f9f7fb714ca3070166fa3a711e117660037de0
yusei53 <yusei530922@gmail.com>
2024-10-01 00:10:35 +0900

    fix: 投稿画面にtiptapを使用

--- app/components/reflection-post/ReflectionPostForm.tsx
+++ app/components/reflection-post/ReflectionPostForm.tsx
@@ -1,5 +1,6 @@
-import { Box, TextField, Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { Controller } from "react-hook-form";
+import Editor from "../editor/Editor";
 
 type RefrectionPostFormProps = {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
@@ -13,48 +14,37 @@ const ReflectionPostForm: React.FC<RefrectionPostFormProps> = ({
   errors,
 }) => {
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <Box display={"flex"} flexDirection={"column"}>
-          <Controller
-            name="title"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id={"title"}
-                label="Title"
-                error={!!errors.title}
-                helperText={errors.title ? errors.title?.message : ""}
-                sx={{ alignSelf: "center", mb: 2 }}
-              />
-            )}
-          />
-          <Controller
-            name="content"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="content"
-                label="Content"
-                multiline
-                rows={4}
-                error={!!errors.content}
-                helperText={errors.content ? errors.content.message : ""}
-                sx={{ alignSelf: "center", mb: 2 }}
-              />
-            )}
-          />
-          <Button
-            type="submit"
-            sx={{ bgcolor: "blue", color: "white", alignSelf: "center" }}
-          >
-            投稿
-          </Button>
-        </Box>
-      </form>
-    </>
+    <form onSubmit={onSubmit}>
+      <Box display={"flex"} flexDirection={"column"}>
+        <Controller
+          name="title"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              id="title"
+              label="Title"
+              error={!!errors.title}
+              helperText={errors.title ? errors.title.message : ""}
+              sx={{ alignSelf: "center", mb: 2 }}
+            />
+          )}
+        />
+        <Controller
+          name="content"
+          control={control}
+          render={({ field }) => (
+            <Editor value={field.value} onChange={field.onChange} />
+          )}
+        />
+        <Button
+          type="submit"
+          sx={{ bgcolor: "blue", color: "white", alignSelf: "center" }}
+        >
+          投稿
+        </Button>
+      </Box>
+    </form>
   );
 };
 
