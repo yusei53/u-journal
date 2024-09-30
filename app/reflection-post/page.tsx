8ec08cfeb7156bca432ba3f25abc404fb68356fb
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-09-30 18:04:39 +0900

    improve: useclientの位置を変更し、不要な記述を削除

--- app/reflection-post/page.tsx
+++ app/reflection-post/page.tsx
@@ -1,7 +1,5 @@
-"use client";
 import getCurrentUser from "../actions/getCurrentUser";
 import ReflectionPostPage from "../components/reflection-post/ReflectionPostPage";
-import reflectionPostsAPI from "../hooks/reflection-post-api";
 
 const page = async () => {
   const currentUser = await getCurrentUser();
