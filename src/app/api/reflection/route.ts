08a3e5825ab723128480a0fdb9f715966d0fd6f5
yusei53 <yusei530922@gmail.com>
2024-10-01 18:29:40 +0900

    change: /api/post→api/reflection

--- src/app/api/reflection/route.ts
+++ src/app/api/reflection/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const allReflectionPosts = await prisma.post.findMany();
-    return NextResponse.json(allReflectionPosts);
+    const response = await prisma.reflection.findMany();
+    return NextResponse.json(response);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "Error get posts" }, { status: 500 });
@@ -22,10 +22,11 @@ export async function POST(req: NextRequest) {
       return new NextResponse("認証されていません", { status: 401 });
     }
 
-    const response = await prisma.post.create({
+    const response = await prisma.reflection.create({
       data: {
         title,
         content,
+        createdAt: new Date(),
         userId: currentUser.id,
       },
     });
