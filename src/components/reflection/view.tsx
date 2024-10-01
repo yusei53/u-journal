f5d610a5b9db5bd3247faba9e9cefcce7ce46749
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-10-01 20:49:24 +0900

    fix: liタグをdivタグに直した

--- src/components/reflection/view.tsx
+++ src/components/reflection/view.tsx
@@ -9,11 +9,11 @@ const DisplayContent = async () => {
       <h1>投稿内容の表示</h1>
       <ul>
         {data.map((post) => (
-          <li key={post.reflectionUUID}>
+          <div key={post.reflectionUUID}>
             <h2>{post.title}</h2>
             <HtmlContent title={post.title} content={post.content} />
             <p>{post.createdAt}</p>
-          </li>
+          </div>
         ))}
       </ul>
     </div>
