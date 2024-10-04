a13e5334d97be660576da6db6a5e81d6596f256e
yusei53 <yusei530922@gmail.com>
2024-10-04 11:24:12 +0900

    hotfix: hydrationErrorを防ぐ

--- src/components/reflection/html.tsx
+++ src/components/reflection/html.tsx
@@ -4,10 +4,21 @@ import { Box } from "@mui/material";
 type HtmlContentProps = {
   title: string;
   content: string;
+  createdAt: string;
 };
 
-const HtmlContent: React.FC<HtmlContentProps> = ({ title, content }) => {
-  return <Box sx={{ mx: 10 }} dangerouslySetInnerHTML={{ __html: content }} />;
+const HtmlContent: React.FC<HtmlContentProps> = ({
+  title,
+  content,
+  createdAt,
+}) => {
+  return (
+    <Box sx={{ mx: 10 }}>
+      <p>{createdAt}</p>
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </Box>
+  );
 };
 
 export default HtmlContent;
