b98e97ad4874566f2cd0794f35cf86782ddb7f91
yusei53 <yusei530922@gmail.com>
2024-10-05 11:03:00 +0900

    fix: SessionProviderからsessionを削除

--- src/providers/NextAuthProvider.tsx
+++ src/providers/NextAuthProvider.tsx
@@ -3,15 +3,11 @@ import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 export const NextAuthProvider = ({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: Session | null;
 }) => {
   return (
-    <SessionProvider session={session} refetchOnWindowFocus={false}>
-      {children}
-    </SessionProvider>
+    <SessionProvider refetchOnWindowFocus={false}>{children}</SessionProvider>
   );
 };
 
