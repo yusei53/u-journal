a2a2684c20c2c7df3cd4ca770083660c7da1703b
yusei53 <yusei530922@gmail.com>
2024-10-06 10:21:15 +0900

    feat: useParamsでusernameからapiを叩く

--- src/hooks/reflection/useReflections.ts
+++ src/hooks/reflection/useReflections.ts
@@ -1,6 +1,7 @@
 import { reflectionAPI } from "@/src/api/reflection-api";
 import { reflectionsKeys } from "@/src/utils/query-key/reflections-keys";
 import { useQuery } from "@tanstack/react-query";
+
 export const useReflections = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: reflectionsKeys.all,
