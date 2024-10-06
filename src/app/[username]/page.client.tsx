9ad19d2af256d811b8851d420e11b40887be74e9
yusei53 <yusei530922@gmail.com>
2024-10-07 00:27:12 +0900

    feat: UserReflectionListPage.tsxの作成

---
+++ src/app/[username]/page.client.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useReflectionsByUsername } from "@/src/hooks/reflection/useReflectionsByUsername";
+import UserReflectionListArea from "@/src/components/reflection/UserReflectionListArea";
+
+const UserReflectionListPage: React.FC = () => {
+  const { username } = useParams<{ username: string }>();
+
+  const {
+    data: reflectionsWithUser,
+    isLoading,
+    error,
+  } = useReflectionsByUsername(username);
+  if (!reflectionsWithUser) {
+    return undefined;
+  }
+
+  if (isLoading) {
+    return <div>読み込み中...</div>;
+  }
+
+  if (error) {
+    // TODO: このエラーメッセージはいろんなところで使い回しできるので共通コンポーネント実装したい
+    return <div>エラーが発生しました: {error.message}</div>;
+  }
+  if (reflectionsWithUser?.reflections.length === 0) {
+    // TODO: このメッセージはデザイン含めもう少し工夫したい
+    return <div>このユーザーはまだ投稿をしていません。</div>;
+  }
+
+  console.log(reflectionsWithUser.userImage);
+
+  return (
+    <UserReflectionListArea
+      userImage={reflectionsWithUser.userImage}
+      username={username}
+      reflections={reflectionsWithUser.reflections}
+    />
+  );
+};
+
+export default UserReflectionListPage;
