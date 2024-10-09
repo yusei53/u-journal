d99a82fa5631db771cebf4d312f7f9da4faad39f
yusei53 <yusei530922@gmail.com>
2024-10-09 18:59:18 +0900

    fix: query-keyを適切なものに変える

--- src/hooks/reflection/useReflections.ts
+++ src/hooks/reflection/useReflections.ts
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useReflections = () => {
   const { data, isLoading, error } = useQuery({
-    queryKey: reflectionsKeys.all,
+    queryKey: reflectionsKeys.lists(),
     queryFn: reflectionAPI.getReflections,
   });
 
