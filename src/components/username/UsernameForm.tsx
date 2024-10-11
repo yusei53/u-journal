cc7327d6362466c588297b203a0e87ff77bd3012
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-10-11 18:36:52 +0900

    feat: usernameにモーダル機能を実装

--- src/components/username/UsernameForm.tsx
+++ src/components/username/UsernameForm.tsx
@@ -1,4 +1,4 @@
-import { Typography, TextField, Button } from "@mui/material";
+import { Typography, TextField, Button, Box, Modal } from "@mui/material";
 import React from "react";
 import { Controller } from "react-hook-form";
 
@@ -8,40 +8,68 @@ type UsernameFormProps = {
   errors: any;
 };
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
 const UsernameForm: React.FC<UsernameFormProps> = ({
   SubmitUsername,
   control,
   errors,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
-      <Typography>ユーザIDを設定してください</Typography>
-      <form onSubmit={SubmitUsername}>
-        <Controller
-          name="username"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              id="username"
-              label="username"
-              error={!!errors.username}
-              helperText={
-                typeof errors.username?.message === "string"
-                  ? errors.username.message
-                  : ""
-              }
-              sx={{ alignSelf: "center", mb: 2 }}
+      <Box bgcolor={"#13396E"} color={"white"}>
+        <Button onClick={handleOpen}>Open modal</Button>
+      </Box>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            ユーザーネームを設定してください
+          </Typography>
+          <form onSubmit={SubmitUsername}>
+            <Controller
+              name="username"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="username"
+                  label="username"
+                  error={!!errors.username}
+                  helperText={
+                    typeof errors.username?.message === "string"
+                      ? errors.username.message
+                      : ""
+                  }
+                  sx={{ alignSelf: "center", mb: 2 }}
+                />
+              )}
             />
-          )}
-        />
-        <Button
-          type="submit"
-          sx={{ bgcolor: "blue", color: "white", alignSelf: "center" }}
-        >
-          設定する
-        </Button>
-      </form>
+            <Button
+              type="submit"
+              sx={{ bgcolor: "blue", color: "white", alignSelf: "center" }}
+            >
+              設定する
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </>
   );
 };
