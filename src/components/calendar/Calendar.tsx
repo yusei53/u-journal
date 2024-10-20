3076b2f2755ad4bd3d149fff8c967f79f310bca6
yusei53 <yusei530922@gmail.com>
2024-10-20 17:45:12 +0900

    rename: Heatmap→Calendar

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,67 +1,38 @@
-import { useReflectionsCount } from "@/src/hooks/reflections-count/useReflectionsCount";
-import { getOneYearAgo } from "@/src/utils/date-helper/date-helpers";
-import Heatmap from "./Heatmap";
-import { getColor } from "@/src/utils/calendar/get-color";
-import { Tooltip } from "react-tooltip";
-import { ReactCalendarHeatmapValue } from "react-calendar-heatmap";
+import CalendarHeatmap, {
+  ReactCalendarHeatmapValue,
+} from "react-calendar-heatmap";
 import { ReflectionPerDate } from "@/src/api/reflections-count-api";
+import "react-calendar-heatmap/dist/styles.css";
+import "@/src/utils/calendar/calendar.css";
 
 type CalendarProps = {
-  username: string;
+  startDate: Date;
+  endDate: Date;
+  values: ReflectionPerDate[];
+  classForValue: (
+    value: ReactCalendarHeatmapValue<string> | undefined
+  ) => string;
+  tooltipDataAttrs: (
+    value: ReactCalendarHeatmapValue<string> | undefined
+  ) => Record<string, string>;
 };
 
-export const Calendar: React.FC<CalendarProps> = ({ username }) => {
-  const {
-    data: reflectionsCount,
-    isLoading,
-    error,
-  } = useReflectionsCount(username);
-  if (!reflectionsCount) return undefined;
-
-  if (isLoading) {
-    return <div>読み込み中...</div>;
-  }
-
-  if (error) {
-    return <div>エラーが発生しました: {error.message}</div>;
-  }
-
-  const startDate = getOneYearAgo();
-  const endDate = new Date();
-
-  const classForValue = (
-    value: ReactCalendarHeatmapValue<string> | undefined
-  ): string => {
-    const reflectionValue = value as ReflectionPerDate | undefined;
-    if (!reflectionValue || !reflectionValue.countReflections) {
-      return "color-empty";
-    }
-    return getColor(reflectionValue.countReflections);
-  };
-
-  const tooltipDataAttrs = (
-    value: ReactCalendarHeatmapValue<string> | undefined
-  ): Record<string, string> => {
-    const reflectionValue = value as ReflectionPerDate | undefined;
-    if (!reflectionValue || !reflectionValue.date) {
-      return {};
-    }
-    return {
-      "data-tooltip-id": "my-tooltip",
-      "data-tooltip-content": `${reflectionValue.date} の投稿数: ${reflectionValue.countReflections}`,
-    };
-  };
-
+const Calendar: React.FC<CalendarProps> = ({
+  startDate,
+  endDate,
+  values,
+  classForValue,
+  tooltipDataAttrs,
+}) => {
   return (
-    <>
-      <Heatmap
-        startDate={startDate}
-        endDate={endDate}
-        values={reflectionsCount.reflectionsPerDate}
-        classForValue={classForValue}
-        tooltipDataAttrs={tooltipDataAttrs}
-      />
-      <Tooltip id="my-tooltip" />
-    </>
+    <CalendarHeatmap
+      startDate={startDate}
+      endDate={endDate}
+      values={values}
+      classForValue={classForValue}
+      tooltipDataAttrs={tooltipDataAttrs}
+    />
   );
 };
+
+export default Calendar;
