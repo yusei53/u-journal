7b0ec83bdc051bf20d8f5949ce770c780414862e
yusei53 <yusei530922@gmail.com>
2024-10-23 21:38:38 +0900

    update: placeholderをMDにつける

--- src/components/markdown-editor/MarkdownEditor.tsx
+++ src/components/markdown-editor/MarkdownEditor.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
+import Placeholder from "@tiptap/extension-placeholder";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Box } from "@mui/material";
@@ -13,11 +14,19 @@ type EditorProps = {
 
 const MarkdownEditor: React.FC<EditorProps> = ({ value, onChange }) => {
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography],
+    extensions: [
+      StarterKit,
+      Highlight,
+      Typography,
+      Placeholder.configure({
+        placeholder: "文章を入力",
+      }),
+    ],
     content: value,
     onUpdate: ({ editor }) => {
       onChange(editor.getHTML());
     },
+
     immediatelyRender: false,
   });
 
