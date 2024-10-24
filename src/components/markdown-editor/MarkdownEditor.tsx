fb0939b9ba6fed7205a9386aeab284028e89c869
yusei53 <yusei530922@gmail.com>
2024-10-25 00:45:51 +0900

    feat: refを渡してEnterで改行できるようにする

--- src/components/markdown-editor/MarkdownEditor.tsx
+++ src/components/markdown-editor/MarkdownEditor.tsx
@@ -1,40 +1,54 @@
-"use client";
+import React, { forwardRef, useImperativeHandle } from "react";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import Placeholder from "@tiptap/extension-placeholder";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Box } from "@mui/material";
 import StyledEditorContent from "./StyledEditorContent";
 
-type EditorProps = {
+type MarkdownEditorProps = {
   value: string;
   onChange: (content: string) => void;
 };
 
-const MarkdownEditor: React.FC<EditorProps> = ({ value, onChange }) => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Highlight,
-      Typography,
-      Placeholder.configure({
-        placeholder: "文章を入力",
-      }),
-    ],
-    content: value,
-    onUpdate: ({ editor }) => {
-      onChange(editor.getHTML());
-    },
-
-    immediatelyRender: false,
-  });
-
-  return (
-    <Box mx={10}>
-      <StyledEditorContent editor={editor} />
-    </Box>
-  );
+export type MarkdownEditorRef = {
+  focus: () => void;
 };
+
+const MarkdownEditor = forwardRef<MarkdownEditorRef, MarkdownEditorProps>(
+  ({ value, onChange }, ref) => {
+    const editor = useEditor({
+      extensions: [
+        StarterKit,
+        Highlight,
+        Typography,
+        Placeholder.configure({
+          placeholder: "文章を入力",
+        }),
+      ],
+      content: value,
+      onUpdate: ({ editor }) => {
+        onChange(editor.getHTML());
+      },
+      immediatelyRender: false,
+    });
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        focus: () => {
+          if (editor) {
+            editor.commands.focus();
+          }
+        },
+      }),
+      [editor]
+    );
+
+    return <StyledEditorContent editor={editor} />;
+  }
+);
+
+MarkdownEditor.displayName = "MarkdownEditor";
 
 export default MarkdownEditor;
