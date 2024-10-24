62522387c6d8fa141d763187a96997c0dc92aa8d
yusei53 <yusei530922@gmail.com>
2024-10-25 00:46:11 +0900

    feat: CustomInputの作成

---
+++ src/components/shared/input/CustomInput.tsx
@@ -0,0 +1,40 @@
+import "./input.css";
+
+type CustomInputProps = {
+  id: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onEnter?: () => void;
+  onCompositionStart?: () => void;
+  onCompositionEnd?: () => void;
+};
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  id,
+  placeholder,
+  value,
+  onChange,
+  onEnter,
+  onCompositionStart,
+  onCompositionEnd,
+}) => {
+  return (
+    <input
+      id={id}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && onEnter) {
+          onEnter();
+        }
+      }}
+      onCompositionStart={onCompositionStart}
+      onCompositionEnd={onCompositionEnd}
+      className="custom-input"
+    />
+  );
+};
+
+export default CustomInput;
