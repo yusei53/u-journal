3c7bbabf880f9a507fee7377df1507f92b25889c
yusei53 <yusei530922@gmail.com>
2024-10-25 20:58:33 +0900

    fix: 投稿ページのUIを統一感持たす

--- src/components/reflection/ReflectionPostForm.tsx
+++ src/components/reflection/ReflectionPostForm.tsx
@@ -1,10 +1,11 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button as MuiButton } from "@mui/material";
 import { Controller } from "react-hook-form";
 import { useRef, useState } from "react";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 import { CustomInput } from "../shared/input";
 import { MarkdownEditor, MarkdownEditorRef } from "../markdown-editor";
+import { Button } from "../shared/button";
 
 const ReflectionPostForm = ({ onSubmit, control, errors }: any) => {
   const [showPicker, setShowPicker] = useState(false);
@@ -22,66 +23,59 @@ const ReflectionPostForm = ({ onSubmit, control, errors }: any) => {
 
   return (
     <form onSubmit={onSubmit}>
-      <Box display="flex" flexDirection="column">
-        <Controller
-          name="title"
-          control={control}
-          render={({ field }) => (
-            <CustomInput
-              id="title"
-              placeholder="タイトル"
-              value={field.value}
-              onChange={field.onChange}
-              onEnter={handleEnterPress}
-              onCompositionStart={() => setIsComposing(true)}
-              onCompositionEnd={() => setIsComposing(false)}
-            />
-          )}
-        />
-        <Controller
-          name="content"
-          control={control}
-          render={({ field }) => (
-            <MarkdownEditor
-              value={field.value}
-              ref={editorRef}
-              onChange={field.onChange}
-            />
-          )}
-        />
-        <Controller
-          name="charStamp"
-          control={control}
-          render={({ field }) => (
-            <Box position="relative">
-              <Button
-                sx={{ fontSize: "24px" }}
-                onClick={() => setShowPicker((prev) => !prev)}
-              >
-                {selectedEmoji}
-              </Button>
-              {showPicker && (
-                <Box position="absolute" zIndex={10}>
-                  <Picker
-                    data={data}
-                    onEmojiSelect={(emoji: any) => {
-                      setSelectedEmoji(emoji.native);
-                      field.onChange(emoji.native);
-                      setShowPicker(false);
-                    }}
-                  />
-                </Box>
-              )}
-            </Box>
-          )}
-        />
-        <Button
-          type="submit"
-          sx={{ bgcolor: "blue", color: "white", alignSelf: "center" }}
-        >
-          投稿
-        </Button>
-      </Box>
+      <Controller
+        name="title"
+        control={control}
+        render={({ field }) => (
+          <CustomInput
+            id="title"
+            placeholder="タイトル"
+            value={field.value}
+            onChange={field.onChange}
+            onEnter={handleEnterPress}
+            onCompositionStart={() => setIsComposing(true)}
+            onCompositionEnd={() => setIsComposing(false)}
+          />
+        )}
+      />
+      <Controller
+        name="content"
+        control={control}
+        render={({ field }) => (
+          <MarkdownEditor
+            value={field.value}
+            ref={editorRef}
+            onChange={field.onChange}
+          />
+        )}
+      />
+      <Controller
+        name="charStamp"
+        control={control}
+        render={({ field }) => (
+          <Box position="relative">
+            <MuiButton
+              sx={{ fontSize: "24px" }}
+              onClick={() => setShowPicker((prev) => !prev)}
+            >
+              {selectedEmoji}
+            </MuiButton>
+            {showPicker && (
+              <Box position="absolute">
+                <Picker
+                  data={data}
+                  onEmojiSelect={(emoji: any) => {
+                    setSelectedEmoji(emoji.native);
+                    field.onChange(emoji.native);
+                    setShowPicker(false);
+                  }}
+                />
+              </Box>
+            )}
+          </Box>
+        )}
+      />
+      <Button type="submit">投稿する</Button>
     </form>
   );
 };
--- src/components/shared/button/Button.tsx
+++ src/components/shared/button/Button.tsx
@@ -1,10 +1,14 @@
 import { Button as MuiButton, ButtonProps } from "@mui/material";
 
 type CustomButtonProps = {
-  text: string;
+  children: React.ReactNode;
 } & ButtonProps;
 
-export const Button: React.FC<CustomButtonProps> = ({ text, sx, ...props }) => (
+export const Button: React.FC<CustomButtonProps> = ({
+  children,
+  sx,
+  ...props
+}) => (
   <MuiButton
     sx={{
       color: "black",
@@ -15,6 +19,6 @@ export const Button: React.FC<CustomButtonProps> = ({ text, sx, ...props }) => (
     }}
     {...props}
   >
-    {text}
+    {children}
   </MuiButton>
 );
