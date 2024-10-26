1e8e0823ad043829c3f212b6b93f51a533501d2a
yusei53 <yusei530922@gmail.com>
2024-10-26 18:51:41 +0900

    fix: page.tsxをクライアントとサーバーに分ける

---
+++ src/app/post/page.client.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useSession } from "next-auth/react";
+import Loading from "../loading";
+import { useRef, useState } from "react";
+import { MarkdownEditorRef } from "@/src/components/reflection/form/markdown-editor";
+import { useCreateReflectionForm } from "@/src/hooks/reflection/useCreateReflectionForm";
+import ReflectionPostForm from "@/src/components/reflection/form/ReflectionPostForm";
+import LoginForm from "@/src/components/auth/LoginForm";
+
+const CreateReflectionPage = () => {
+  const { data: session, status } = useSession();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const editorRef = useRef<MarkdownEditorRef>(null);
+  const { control, errors, onSubmit } = useCreateReflectionForm(
+    session?.user.username
+  );
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    await onSubmit(e);
+    setIsLoading(false);
+  };
+
+  const handleEnter = () => {
+    if (editorRef.current) {
+      editorRef.current.focus();
+    }
+  };
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  {
+    return session ? (
+      <ReflectionPostForm
+        control={control}
+        errors={errors}
+        isLoading={isLoading}
+        onSubmit={handleSubmit}
+        onEnter={handleEnter}
+        editorRef={editorRef}
+      />
+    ) : (
+      <LoginForm />
+    );
+  }
+};
+
+export default CreateReflectionPage;
--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-import ReflectionPostPage from "@/src/components/pages/ReflectionPostPage";
-import { useSession } from "next-auth/react";
-import Loading from "../loading";
+import CreateReflectionPage from "./page.client";
 
-const Page = () => {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") {
-    return <Loading />;
-  }
-
-  {
-    return session ? (
-      <ReflectionPostPage
-        currentUserId={session.user.id}
-        username={session.user.username}
-      />
-    ) : (
-      <div>ddd</div>
-    );
-  }
+const page = () => {
+  return <CreateReflectionPage />;
 };
 
-export default Page;
+export default page;
