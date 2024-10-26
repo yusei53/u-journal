b860c33506de6dde9cf0bd0efdeb3dcb6f143b7b
yusei53 <yusei530922@gmail.com>
2024-10-26 18:32:46 +0900

    feat: useCreateReflectionFormを作成

---
+++ src/hooks/reflection/useCreateReflectionForm.tsx
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+import { useCreateReflection } from "@/src/hooks/reflection/useCreateReflection";
+
+export const createReflectionSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "タイトルは1文字以上で入力してください。" }),
+  content: z
+    .string()
+    .min(3, { message: "本文は3文字以上で入力してください。" }),
+  charStamp: z.string(),
+});
+
+export type CreateReflectionSchemaType = z.infer<typeof createReflectionSchema>;
+
+export const useCreateReflectionForm = (username: string | undefined) => {
+  const router = useRouter();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<CreateReflectionSchemaType>({
+    resolver: zodResolver(createReflectionSchema),
+    defaultValues: { title: "", content: "", charStamp: "💭" },
+  });
+
+  const createReflectionMutation = useCreateReflection(username ?? ""); // usernameがundefinedの場合があるため
+
+  const onSubmit = handleSubmit((formData: CreateReflectionSchemaType) => {
+    createReflectionMutation.mutate(formData, {
+      onSuccess: () => router.push(`/${username}`),
+    });
+  });
+
+  return {
+    control,
+    errors,
+    onSubmit,
+  };
+};
