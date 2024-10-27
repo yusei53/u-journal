d3bad7759bb9a74fad05eb660bd2ab1b5a5457e5
yusei53 <yusei530922@gmail.com>
2024-10-27 16:07:34 +0900

    feat: PublishStatusOptionButtonの作成

---
+++ src/components/reflection/form/popup/PublishStatusOptionButton.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/src/components/shared/button";
+import { theme } from "@/src/utils/theme/theme";
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import CheckIcon from "@mui/icons-material/Check";
+
+type PublishStatusOptionButtonProps = {
+  isActive: boolean;
+  onClick: () => void;
+  icon: string;
+  text: string;
+  description: string;
+};
+
+const PublishStatusOptionButton: React.FC<PublishStatusOptionButtonProps> = ({
+  isActive,
+  onClick,
+  icon,
+  text,
+  description,
+}) => (
+  <Button
+    onClick={onClick}
+    sx={{
+      border: "none",
+      display: "block",
+      textAlign: "left",
+      width: "100%",
+      borderRadius: "none",
+      "&:hover": { backgroundColor: theme.palette.primary.contrastText },
+    }}
+    disableRipple
+  >
+    <Box display={"flex"} alignItems={"center"} color={"black"}>
+      <Image
+        src={icon}
+        alt={`${text} Icon`}
+        width={18}
+        height={18}
+        style={{ marginRight: 4 }}
+      />
+      {text}
+      {isActive && <CheckIcon fontSize="small" sx={{ ml: 1 }} />}
+    </Box>
+    <Typography fontSize={12} color={theme.palette.grey[500]}>
+      {description}
+    </Typography>
+  </Button>
+);
+
+export default PublishStatusOptionButton;
