5654380962469dd9eaaa358051a98265e0acab87
yusei53 <yusei530922@gmail.com>
2024-11-02 10:58:18 +0900

    コンポーネント名をアップデート

---
+++ src/components/user-reflection-list/profile/UserProfileArea.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import { UserAvatar } from "./avatar";
+import { CalendarFetcher } from "./calendar";
+
+type UserProfileAreaProps = {
+  userImage: string;
+  username: string;
+};
+
+const UserProfileArea: React.FC<UserProfileAreaProps> = ({
+  userImage,
+  username,
+}) => {
+  return (
+    <>
+      <UserAvatar userImage={userImage} username={username} />
+      <Box my={5}>
+        <CalendarFetcher username={username} />
+      </Box>
+    </>
+  );
+};
+
+export default UserProfileArea;
