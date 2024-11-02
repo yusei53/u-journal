8a8af4a85792e9bf5b706a40e183a1f921f03882
yusei53 <yusei530922@gmail.com>
2024-11-02 15:47:15 +0900

    fix: uiをアップデート

--- src/components/user-reflection-list/profile/calendar/Calendar.tsx
+++ src/components/user-reflection-list/profile/calendar/Calendar.tsx
@@ -1,9 +1,12 @@
-import CalendarHeatmap, {
-  ReactCalendarHeatmapValue,
-} from "react-calendar-heatmap";
+import { Box, Typography } from "@mui/material";
 import { ReflectionPerDate } from "@/src/api/reflections-count-api";
 import "react-calendar-heatmap/dist/styles.css";
 import "@/src/utils/calendar/calendar.css";
+import { theme } from "@/src/utils/theme";
+import CustomCalendarHeatmap from "./CustomCalendarHeatmap";
+import { ReactCalendarHeatmapValue } from "react-calendar-heatmap";
+import { IOSSwitch } from "@/src/components/shared/switch";
+import { useToggleJapaneseLabels } from "@/src/hooks/calendar/useToggleJapaneseLabels";
 
 type CalendarProps = {
   startDate: Date;
@@ -24,14 +27,32 @@ const Calendar: React.FC<CalendarProps> = ({
   classForValue,
   tooltipDataAttrs,
 }) => {
+  const { calendarRef, handleToggleLabels } = useToggleJapaneseLabels();
+
   return (
-    <CalendarHeatmap
-      startDate={startDate}
-      endDate={endDate}
-      values={values}
-      classForValue={classForValue}
-      tooltipDataAttrs={tooltipDataAttrs}
-    />
+    <Box
+      ref={calendarRef}
+      border={`1px solid ${theme.palette.grey[400]}`}
+      borderRadius={2}
+      p={2}
+      pl={0.5}
+    >
+      <Box textAlign={"right"} mb={1.2}>
+        <Typography component={"span"} fontSize={11} mr={0.8}>
+          日本語表示
+        </Typography>
+        <IOSSwitch onClick={handleToggleLabels} />
+      </Box>
+      <CustomCalendarHeatmap
+        startDate={startDate}
+        endDate={endDate}
+        values={values}
+        classForValue={classForValue}
+        tooltipDataAttrs={tooltipDataAttrs}
+        showWeekdayLabels
+        gutterSize={2}
+      />
+    </Box>
   );
 };
 
