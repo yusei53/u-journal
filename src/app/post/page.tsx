83937243d81968925b362b308444d233ee7678e0
yusei53 <yusei530922@gmail.com>
2024-11-05 20:23:37 +0900

    fix: ReflectionPostFormPage

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,36 @@
-import CreateReflectionPage from "./page.client";
+"use client";
+import { useCreateReflectionForm } from "@/src/hooks/reflection/useCreateReflectionForm";
+import ReflectionPostForm from "@/src/components/form/ReflectionPostForm";
+import LoginForm from "@/src/components/auth/LoginForm";
+import { useSession } from "next-auth/react";
+import Loading from "../loading";
 
-const page = () => {
-  return <CreateReflectionPage />;
+const ReflectionPostFormPage = () => {
+  const { data: session, status } = useSession();
+
+  const { control, isSubmitting, errors, onSubmit } = useCreateReflectionForm(
+    session?.user.username
+  );
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await onSubmit(e);
+  };
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  return session?.user.username ? (
+    <ReflectionPostForm
+      control={control}
+      isSubmitting={isSubmitting}
+      errors={errors}
+      onSubmit={handleSubmit}
+    />
+  ) : (
+    <LoginForm />
+  );
 };
 
-export default page;
+export default ReflectionPostFormPage;
