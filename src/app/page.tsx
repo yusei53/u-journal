b438f28f725ca06d980d5da5c7cd55c84191bdae
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-11-05 18:51:10 +0900

    add: toppageにReflectionAllPageを表示させた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import LoginForm from "../components/auth/LoginForm";
 import LogoutButton from "../components/auth/LogoutButton";
 import { useSession } from "next-auth/react";
-import { IconButton, Tooltip } from "@mui/material";
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import ReflectionCardWithIconArea from "../components/reflection-all/ReflectionCardWithIconArea";
+import ReflectionAllPage from "../components/pages/ReflectionAllPage";
 
-const Home = () => {
+const Home = async ({ params }: { params: { username: string } }) => {
+  const { username } = params;
   const { data: session, status } = useSession();
 
   if (status === "loading") {
@@ -25,32 +26,7 @@ const Home = () => {
           />
         </div>
         <LogoutButton />
-        <Tooltip
-          title={"振り返りをする"}
-          slotProps={{
-            popper: {
-              modifiers: [
-                {
-                  name: "offset",
-                  options: {
-                    offset: [0, -10],
-                  },
-                },
-              ],
-            },
-          }}
-        >
-          <IconButton
-            aria-label="振り返りをする"
-            sx={{
-              border: "1px solid #DCDFE3",
-              borderRadius: 5,
-            }}
-            href="/post"
-          >
-            <AddOutlinedIcon />
-          </IconButton>
-        </Tooltip>
+        <ReflectionAllPage />
       </>
     ) : (
       <>
