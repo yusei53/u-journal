59dac71a40d67de518b2060f02ee91a061db1a8b
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-11-05 18:49:52 +0900

    add: ReflectionCardWithIconArea.tsxを作成

---
+++ src/components/reflection-all/ReflectionCardWithIconArea.tsx
@@ -0,0 +1,30 @@
+import Grid from "@mui/material/Grid2";
+import { Reflection, ReflectionAll } from "@/src/api/reflection-api";
+import ReflectionCardWithIcon from "./ReflectionCardWithIcon";
+
+type ReflectionCardListAreaProps = {
+  reflections: ReflectionAll[];
+};
+
+const ReflectionCardListArea: React.FC<ReflectionCardListAreaProps> = ({
+  reflections,
+}) => {
+  return (
+    <Grid container>
+      {reflections.map((reflection) => (
+        <Grid
+          size={{ xs: 12, sm: 6, md: 4 }}
+          key={reflection.reflectionCUID}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          mb={2}
+        >
+          <ReflectionCardWithIcon reflection={reflection} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ReflectionCardListArea;
