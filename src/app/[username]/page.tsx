467b06f9d73ea69b578aecbc2d2d1fa3be9514b6
yusei53 <yusei530922@gmail.com>
2024-11-10 15:58:28 +0900

    add: generateMetadata

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -2,6 +2,50 @@ import { reflectionsCountAPI } from "@/src/api/reflections-count-api";
 import UserReflectionListPage from "./page.client";
 import { reflectionAPI } from "@/src/api/reflection-api";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import opengraphAPI from "@/src/api/opengraph-api";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { username: string };
+}): Promise<Metadata> => {
+  const { username } = params;
+  const userInformation = await opengraphAPI.getOGPByUsername(username);
+  if (userInformation === 404) {
+    return {
+      title: "404 | u-journal",
+      description: "このページは見つかりません",
+      openGraph: {
+        type: "website",
+        title: "404 | u-journal",
+        description: "存在しないユーザーです",
+        siteName: "u-journal",
+      },
+    };
+  }
+
+  return {
+    title: `${username} | Overview`,
+    description: `${username} has ${userInformation.totalReflections} reflections`,
+    openGraph: {
+      type: "website",
+      url: `https://u-journal.vercel.app/${username}`,
+      title: `${username} | Overview`,
+      description: `${username} has ${userInformation.totalReflections} reflections`,
+      siteName: "u-journal",
+    },
+    twitter: {
+      title: `${username} | Overview`,
+      description: `${username} has ${userInformation.totalReflections} reflections`,
+      card: "summary",
+      images: {
+        url: userInformation.user.image,
+        alt: `${username}'s profile image`,
+      },
+    },
+  };
+};
 
 const page = async ({ params }: { params: { username: string } }) => {
   const { username } = params;
