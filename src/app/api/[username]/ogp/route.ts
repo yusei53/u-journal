9a542521a6775a5c1397573cd5e604964f70eec4
yusei53 <yusei530922@gmail.com>
2024-11-10 15:58:02 +0900

    feat: GET API

---
+++ src/app/api/[username]/ogp/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/src/lib/prisma";
+import { getUserIdByUsername } from "@/src/utils/actions/get-userId-by-username";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { username: string } }
+) {
+  try {
+    const { username } = params;
+
+    const userId = await getUserIdByUsername(username);
+
+    if (!userId) {
+      return NextResponse.json(
+        { message: "ユーザーが見つかりません" },
+        { status: 404 }
+      );
+    }
+
+    const totalReflections = await prisma.reflection.count({
+      where: {
+        userId,
+      },
+    });
+
+    const image = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        image: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        totalReflections,
+        user: image,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching contributions:", error);
+    return NextResponse.json(
+      { message: "Contributions data could not be retrieved" },
+      { status: 500 }
+    );
+  }
+}
