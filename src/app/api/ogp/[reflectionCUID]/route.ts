75ead815025e0524845e1f0699c8b2f3965bada4
yusei53 <yusei530922@gmail.com>
2024-11-10 14:12:26 +0900

    feat: GET API

---
+++ src/app/api/ogp/[reflectionCUID]/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/src/lib/prisma";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { reflectionCUID: string } }
+) {
+  try {
+    const { reflectionCUID } = params;
+
+    const reflection = await prisma.reflection.findUnique({
+      where: {
+        reflectionCUID,
+      },
+      select: {
+        title: true,
+        user: {
+          select: { image: true, username: true },
+        },
+      },
+    });
+
+    if (!reflection) {
+      return NextResponse.json(
+        { message: "Reflection not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      reflection,
+    });
+  } catch (error) {
+    console.error("Error fetching reflection:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
