89c284ef744979d0964fbd0cc2e6c10032a84615
yusei53 <yusei530922@gmail.com>
2024-11-10 17:54:27 +0900

    fix: ogpの画像

--- src/app/[username]/[reflectionCUID]/opengraph-image.tsx
+++ src/app/[username]/[reflectionCUID]/opengraph-image.tsx
@@ -54,7 +54,7 @@ type OgImageProps = {
 const OgImage = ({ userImage, title, username }: OgImageProps) => (
   <div
     style={{
-      backgroundImage: `url(${process.env.NEXT_PUBLIC_API_URL}/ogpp.png)`,
+      backgroundImage: `url(${process.env.NEXT_PUBLIC_API_URL}/ogp.png)`,
       backgroundSize: "100% 100%",
       height: "100%",
       width: "100%",
