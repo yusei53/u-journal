3bc8affc5511caa38475711a4559cdc2cfa1bbc1
yusei53 <yusei530922@gmail.com>
2024-11-14 11:49:33 +0900

    fix: 投稿作成ページのogp

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -2,8 +2,20 @@ import { Metadata } from "next";
 import ReflectionPostFormPage from "./page.client";
 import getCurrentUser from "@/src/utils/actions/get-current-user";
 
+const description = "u-journalの投稿作成ページ";
 export const metadata: Metadata = {
   title: "投稿作成",
+  description: description,
+  openGraph: {
+    type: "website",
+    url: "https://u-journal.vercel.app/post",
+    title: "投稿作成 | u-journal",
+    description: description,
+  },
+  twitter: {
+    title: "投稿作成 | u-journal",
+    description: description,
+  },
 };
 
 const page = async () => {
