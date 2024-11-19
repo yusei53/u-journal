05d4171b3b7cdc0fa71df73dfb8eda2950aed40e
yusei53 <yusei530922@gmail.com>
2024-11-19 11:19:44 +0900

    詳細ページの修正

--- src/components/reflection-detail/ReflectionDetail.tsx
+++ src/components/reflection-detail/ReflectionDetail.tsx
@@ -34,40 +34,37 @@ export const ReflectionDetail: React.FC<ReflectionDetailProps> = ({
     //TODO: ContainerとStackの部分はReflectionPostForm.tsxと同じなので、共通化を検討
     <Container maxWidth="sm" sx={{ my: 15 }}>
       <Stack m={{ md: 2 }}>
-        <Box>
-          <Box display={"flex"} alignItems={"center"} mt={0.5}>
-            <AccessTimeIcon color={"disabled"} sx={{ mr: 0.5, fontSize: 13 }} />
-            <Typography
-              fontSize={13}
-              component={"time"}
-              color={theme.palette.grey[500]}
+        <Box mb={3}>
+          <Box display={"flex"} alignItems={"center"} my={1}>
+            <Link
+              href={`/${username}`}
+              style={{
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
+              <Image
+                src={userImage}
+                alt={`${username}の画像`}
+                width={25}
+                height={25}
+                priority
+                style={{ borderRadius: 100, marginRight: 8 }}
+              />
+              <Typography sx={link}>{username}</Typography>
+            </Link>
+            <AccessTimeIcon
+              color={"disabled"}
+              sx={{ ml: 1.5, mr: 0.5, fontSize: 13 }}
+            />
+            <Typography component={"time"} color={theme.palette.grey[600]}>
               {formatDate(createdAt)}
             </Typography>
           </Box>
           <Typography component={"h1"} className="custom-input">
             {title}
           </Typography>
-        </Box>
-        <Box mt={6} mb={3.5}>
-          <Link
-            href={`/${username}`}
-            style={{
-              textDecoration: "none",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              src={userImage}
-              alt={`${username}の画像`}
-              width={25}
-              height={25}
-              priority
-              style={{ borderRadius: 100, marginRight: 8 }}
-            />
-            <Typography sx={link}>{username}</Typography>
-          </Link>
         </Box>
         <StyledMarkdown
           dangerouslySetInnerHTML={{ __html: content }}
