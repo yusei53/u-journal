d8c76611f29f6225f75e43f14dd2f87e8201bdbd
yusei53 <yusei530922@gmail.com>
2024-11-19 11:36:55 +0900

    rename: user-reflection-list→reflection-list

--- src/app/[username]/page.client.tsx
+++ src/app/[username]/page.client.tsx
@@ -1,6 +1,6 @@
 "use client";
-import UserProfileArea from "@/src/components/user-reflection-list/profile/UserProfileArea";
-import ReflectionCardListArea from "@/src/components/user-reflection-list/reflection-list/ReflectionCardListArea";
+import UserProfileArea from "@/src/components/reflection-list/profile/UserProfileArea";
+import ReflectionCardListArea from "@/src/components/reflection-list/reflection-list/ReflectionCardListArea";
 import { Reflection } from "@/src/api/reflection-api";
 import { ReflectionsCount } from "@/src/api/reflections-count-api";
 
