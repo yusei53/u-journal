902ede4d50e9e4cc3c3ac368d8bd2ce65c73d687
yusei53 <yusei530922@gmail.com>
2024-11-20 18:17:13 +0900

    fix: title

--- src/components/reflection-detail/ReflectionDetail.tsx
+++ src/components/reflection-detail/ReflectionDetail.tsx
@@ -28,7 +28,7 @@ const link = {
 const h1 = {
   p: 0,
   width: "100%",
-  fontSize: "22px",
+  fontSize: "21px",
   border: "none",
   outline: "none",
 };
@@ -41,49 +41,44 @@ export const ReflectionDetail: React.FC<ReflectionDetailProps> = ({
   createdAt,
 }) => {
   return (
-    //TODO: ContainerとStackの部分はReflectionPostForm.tsxと同じなので、共通化を検討
-    <Box sx={{ ...animation(0.6), my: 10, mx: { xs: 1, md: 12 } }}>
-      <Stack my={{ md: 2 }}>
-        <Box mb={2}>
-          <Box display={"flex"} alignItems={"center"} my={1}>
-            <Link
-              href={`/${username}`}
-              style={{
-                textDecoration: "none",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <Image
-                src={userImage}
-                alt={`${username}の画像`}
-                width={25}
-                height={25}
-                priority
-                style={{ borderRadius: 10, marginRight: 8 }}
-              />
-              <Typography sx={link}>{username}</Typography>
-            </Link>
-            <CalendarTodayIcon
-              sx={{
-                color: theme.palette.grey[600],
-                ml: 1.5,
-                mr: 0.5,
-                fontSize: 13,
-              }}
+    <Box my={10} mx={{ xs: 0.5, md: 12 }} sx={{ ...animation(0.6) }}>
+      <Box mb={3}>
+        <Box display={"flex"} alignItems={"center"} my={1}>
+          <Link
+            href={`/${username}`}
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={userImage}
+              alt={`${username}の画像`}
+              width={25}
+              height={25}
+              priority
+              style={{ borderRadius: 10, marginRight: 8 }}
             />
-            <Typography component={"time"} color={theme.palette.grey[600]}>
-              {formatDate(createdAt)}
-            </Typography>
-          </Box>
-          <Typography component={"h1"} sx={h1}>
-            {title}
+            <Typography sx={link}>{username}</Typography>
+          </Link>
+          <CalendarTodayIcon
+            sx={{
+              color: theme.palette.grey[600],
+              ml: 1.5,
+              mr: 0.5,
+              fontSize: 13,
+            }}
+          />
+          <Typography component={"time"} color={theme.palette.grey[600]}>
+            {formatDate(createdAt)}
           </Typography>
         </Box>
-        <StyledMarkdown
-          dangerouslySetInnerHTML={{ __html: content }}
-        ></StyledMarkdown>
-      </Stack>
+        <Typography component={"h1"} sx={h1}>
+          {title}
+        </Typography>
+      </Box>
+      <StyledMarkdown dangerouslySetInnerHTML={{ __html: content }} />
     </Box>
   );
 };
