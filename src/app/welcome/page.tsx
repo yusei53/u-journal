7a0d0a40e3f158488c8ff8efd1590d2dfaa0066b
yusei53 <yusei530922@gmail.com>
2024-11-22 01:09:06 +0900

    feat: /welcome

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,13 @@
+import FirstView from "@/src/components/welcome/first-view/FirstView";
+import SecondView from "@/src/components/welcome/second-view/SecondView";
+
+const page = () => {
+  return (
+    <>
+      <FirstView />
+      <SecondView />
+    </>
+  );
+};
+
+export default page;
