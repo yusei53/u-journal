bfd19b84da77e37a2b541e993d14742ab48906d5
yusei53 <yusei530922@gmail.com>
2024-11-22 01:25:46 +0900

    fix: ogp

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,5 +1,23 @@
 import FirstView from "@/src/components/welcome/first-view/FirstView";
 import SecondView from "@/src/components/welcome/second-view/SecondView";
+import { Metadata } from "next";
+
+const description =
+  "リフティは、日々の振り返りを手助けする振り返りプラットフォームです";
+export const metadata: Metadata = {
+  title: "リフティとは",
+  description: description,
+  openGraph: {
+    type: "website",
+    url: "https://www.refty.jp/welcome",
+    title: "リフティとは | リフティ",
+    description: description,
+  },
+  twitter: {
+    title: "リフティとは | リフティ",
+    description: description,
+  },
+};
 
 const page = () => {
   return (
