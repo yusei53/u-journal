67da057ca9549891f5e8d8637b3b1107e27edc90
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2024-11-28 11:22:01 +0900

    fix:css

--- src/components/form/popup/reflection-template/ReflectionTemplateInstruct.tsx
+++ src/components/form/popup/reflection-template/ReflectionTemplateInstruct.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import { theme } from "@/src/utils/theme";
 
 type ReflectionTemplateInstructProps = {
@@ -12,38 +12,57 @@ export const ReflectionTemplateInstruct: React.FC<
   ReflectionTemplateInstructProps
 > = ({ title, description, fadeOut = false }) => {
   const [isVisible, setIsVisible] = useState(true);
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const pointerPosition: "top" | "right" = isMobile ? "top" : "right";
+
   if (fadeOut && isVisible) {
     setTimeout(() => setIsVisible(false), 1000);
   }
 
-  const pointerStyles = {
-    "&::after": {
-      content: '""',
-      position: "absolute",
-      right: "-10px",
-      top: "20px",
-      transform: "translateY(-50%)",
-      width: 0,
-      height: 0,
-      borderStyle: "solid",
-      borderWidth: "10px 0 10px 10px",
-      borderColor: `transparent transparent transparent ${theme.palette.grey[400]}`,
+  const pointerStyles: Record<"top" | "right", any> = {
+    top: {
+      "&::after": {
+        content: '""',
+        position: "absolute",
+        top: "-10px",
+        left: "25%",
+        transform: "translateX(-50%)",
+        width: 0,
+        height: 0,
+        borderStyle: "solid",
+        borderWidth: "0 10px 10px 10px",
+        borderColor: `transparent transparent ${theme.palette.grey[400]} transparent`,
+      },
+    },
+    right: {
+      "&::after": {
+        content: '""',
+        position: "absolute",
+        right: "-10px",
+        top: "50%",
+        transform: "translateY(-50%)",
+        width: 0,
+        height: 0,
+        borderStyle: "solid",
+        borderWidth: "10px 0 10px 10px",
+        borderColor: `transparent transparent transparent ${theme.palette.grey[400]}`,
+      },
     },
   };
 
   return (
-    <Box display={"flex"} py={2}>
+    <Box display={"flex"} px={2}>
       <Box
         position={"relative"}
         bgcolor={theme.palette.grey[400]}
-        borderRadius={"8px"}
-        padding={"16px"}
-        maxWidth={"300px"}
+        borderRadius={4}
+        padding={2}
+        maxWidth={{ xs: 200, md: 500 }}
         sx={{
-          ...pointerStyles,
           opacity: isVisible ? 1 : 0,
-          visibility: isVisible ? "visible" : "hidden",
-          transition: "opacity 0.5s ease, visibility 0.5s ease",
+          transition: "opacity 0.5s ease",
+          ...pointerStyles[pointerPosition],
         }}
       >
         <Typography fontWeight={"bold"}>{title}</Typography>
