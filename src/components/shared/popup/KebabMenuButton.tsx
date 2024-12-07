38ceeb16558478511a4daab0b71082af5bdf2c47
na2kera <nat3mitaro@gmail.com>
2024-12-07 14:01:55 +0900

    UIの作成

--- src/components/shared/popup/KebabMenuButton.tsx
+++ src/components/shared/popup/KebabMenuButton.tsx
@@ -2,6 +2,8 @@ import { theme } from "@/src/utils/theme";
 import { Box, Popper, Fade, SxProps } from "@mui/material";
 import Image from "next/image";
 import { Button } from "../button";
+import { useState } from "react";
+import { DeleteConfirmationModal } from "../../reflection-list/modal/DeleteConfirmationModal";
 
 type KebabMenuButtonProps = {
   anchorEl: HTMLElement | null;
@@ -22,6 +24,16 @@ export const KebabMenuButton: React.FC<KebabMenuButtonProps> = ({
   reflectionCUID,
   sx,
 }) => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const handleDeleteModalOpen = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteModalClose = () => {
+    setIsDeleteModalOpen(false);
+  };
+
   return (
     <>
       <Box
@@ -79,10 +91,38 @@ export const KebabMenuButton: React.FC<KebabMenuButtonProps> = ({
                   編集する
                 </Box>
               </Button>
+              <Button
+                onClick={handleDeleteModalOpen}
+                sx={{
+                  border: "none",
+                  display: "block",
+                  textAlign: "left",
+                  borderRadius: "none",
+                  "&:hover": {
+                    backgroundColor: theme.palette.primary.contrastText,
+                  },
+                }}
+              >
+                <Box display={"flex"} alignItems={"center"} letterSpacing={0.8}>
+                  <Image
+                    src={"/delete.svg"}
+                    alt={`Icon`}
+                    width={18}
+                    height={18}
+                    style={{ marginRight: 10 }}
+                  />
+                  削除する
+                </Box>
+              </Button>
             </Box>
           </Fade>
         )}
       </Popper>
+      <DeleteConfirmationModal
+        open={isDeleteModalOpen}
+        onClose={handleDeleteModalClose}
+        reflectionCUID={reflectionCUID}
+      />
     </>
   );
 };
