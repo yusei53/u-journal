869e69e75926fecab97b7059c3605b73ee6b2ddd
yusei53 <yusei530922@gmail.com>
2024-12-15 16:36:12 +0900

    fix: use userImage and active icon

--- src/components/shared/footer/mobile/MobileFooterButton.tsx
+++ src/components/shared/footer/mobile/MobileFooterButton.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { theme } from "@/src/utils/theme";
 
 type MobileFooterButtonProps = {
   href: string;
@@ -15,15 +17,33 @@ export const MobileFooterButton: React.FC<MobileFooterButtonProps> = ({
   alt,
   isAvatar = false,
 }) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  const defaultImagePass = `/mobile-footer/${imagePass}`;
+  const activeImagePass = `/mobile-footer/active-${imagePass}`;
+
+  // NOTE: アバターなら/mobile~ではなく、通常パス。そうじゃないならactivePageの場合とそうじゃない場合でパスを出し分ける
+  const displayedImage = isAvatar
+    ? imagePass
+    : isActive
+    ? activeImagePass
+    : defaultImagePass;
+
   return (
     <Box textAlign={"center"} lineHeight={0.8}>
       <Link href={href} style={{ textDecoration: "none" }}>
         <Image
-          src={isAvatar ? `${imagePass}` : `/mobile-footer/${imagePass}`}
+          src={displayedImage}
           alt={alt}
           width={30}
           height={30}
-          style={{ borderRadius: 15 }}
+          style={{
+            borderRadius: 15,
+            border:
+              isAvatar && isActive
+                ? `1px solid ${theme.palette.grey[600]}`
+                : undefined,
+          }}
         />
       </Link>
     </Box>
